42767ab721429635e5567a17216ad0b297c47286
Pragada N D S Sai Srinivas <121677119+srinivasnani005@users.noreply.github.com>
2024-06-27 02:31:30 +0530

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2023 Designed by <a href="https://github.com/srinivasnani005" target="_blank" rel="noopener noreferrer">@Srinivas Nani</a></p>
+</div>
